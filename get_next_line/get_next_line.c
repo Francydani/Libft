1254503c3cf4b22895ec382e8eebdc6e66680d25
Francydani <tegnomina@gmail.com>
2026-05-25 16:36:58 +0200

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,5 +1,19 @@
-#include <>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: francysa <francysa@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/25 11:32:48 by francysa          #+#    #+#             */
+/*   Updated: 2026/05/25 16:12:01 by francysa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <fcntl.h>   // Para open
 #include <unistd.h>
+#include <stdlib.h>
 
 char *get_next_line(int fd)
 {
@@ -9,18 +23,35 @@ char *get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	linea_sin_leer = read(fd, linea_sin_leer, &bytes_leidos);
+	linea_sin_leer = read(fd, linea_sin_leer, &byts_leidos);
 	if (!linea_sin_leer)
 		return (NULL);
-	linea;
-	linea = '\0';
-	
-	;
-	
-	if (esta == '\n')
-	{
-		
 
+
+	if (linea_sin_leer < '\n')
+	{
+        ft_putstr_len(linea_sin_leer);
 	}
-	while( <= '\n')
+    return (0);
+}
+
+int	main(void)
+{
+	int		fd;
+	char	buffer[1500];
+	size_t	bytes_leidos;
+
+	fd = open("archivo.txt", O_RDONLY);
+	bytes_leidos = get_next_line(fd);
+
+	while (bytes_leidos >= '\0')
+	{
+		bytes_leidos++;
+	}
+
+	buffer[bytes_leidos] = '\0';
+	printf("Leídos %zd bytes. Contenido: %s\n", bytes_leidos, buffer);
+	close(fd);
+
+	return (0);
 }
