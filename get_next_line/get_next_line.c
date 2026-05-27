d22172e100a7ba3a102a5eb0e6a405836a53d8e0
Francydani <tegnomina@gmail.com>
2026-05-27 10:08:58 +0200

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,56 +10,110 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 #include <stdio.h>
-#include <fcntl.h>   // Para open
-#include <unistd.h>
-#include <stdlib.h>
+#include <fcntl.h>
 
-char	*ft_obtener_linea(char linea_sin_leer)
+char	*ft_obtener_linea(char *linea_sin_leer)
 {
 	size_t	i;
 	char	*temporal;
 
-	i = ft_strlen(linea_sin_leer);
-	temporal = malloc(i);
+	i = 0;
+	if (!linea_sin_leer || !linea_sin_leer[i])
+		return (NULL);
+	while (linea_sin_leer[i] && linea_sin_leer[i] != '\n')
+		i++;
+	if (linea_sin_leer[i] == '\n')
+		temporal = malloc(i + 2);
+	else
+		temporal = malloc(i + 1);
+	if (!temporal)
+		return (NULL);
+	i = 0;
+	while (linea_sin_leer[i] && linea_sin_leer[i] != '\n')
+	{
+		temporal[i] = linea_sin_leer[i];
+		i++;
+	}
+	if (linea_sin_leer[i] == '\n')
+		temporal[i++] = '\n';
+	temporal[i] = '\0';
 	return (temporal);
+}
+
+char	*ft_clean(char *linea_sin_leer)
+{
+	size_t	i;
+	size_t	j;
+	char	*nueva;
+
+	i = 0;
+	j = 0;
+	if (!linea_sin_leer)
+		return (NULL);
+	while (linea_sin_leer[i] && linea_sin_leer[i] != '\n')
+		i++;
+	if (!linea_sin_leer[i])
+	{
+		free(linea_sin_leer);
+		return (NULL);
+	}
+	nueva = malloc(ft_strlen(linea_sin_leer) - i + 1);
+	if (!nueva)
+		return (NULL);
+	i++;
+	while (linea_sin_leer[i])
+		nueva[j++] = linea_sin_leer[i++];
+	nueva[j] = '\0';
+	free(linea_sin_leer);
+	return (nueva);
 }
 
 char	*get_next_line(int fd)
 {
-	char		*buffer_temporal;
+	char		*buffer;
+	char		*linea_final;
 	static char	*linea_sin_leer;
-	size_t		byts_leidos;
+	long		byts_leidos;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer_temporal = malloc(BUFFER_SIZE + 1);
-	if (buffer_temporal == NULL)
-		return (0);
-	while (buffer_temporal)
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (buffer == NULL)
+		return (NULL);
+	byts_leidos = 1;
+	while (!ft_strchr(linea_sin_leer, '\n') && byts_leidos > 0)
 	{
-		byts_leidos = read(fd, buffer_temporal, BUFFER_SIZE);
-		if (byts_leidos <= 0)
-			return (NULL);
-		buffer_temporal[byts_leidos] = '\0';
-		if (!linea_sin_leer)
-			linea_sin_leer = ft_strdup("");
-		while (!ft_strchr(linea_sin_leer, '\n') && bytes_leidos > 0)
-			linea_sin_leer = ft_strjoin(linea_sin_leer, buffer_temporal);
+		byts_leidos = read(fd, buffer, BUFFER_SIZE);
+		if (byts_leidos == -1)
+			return (free(buffer), NULL);
+		buffer[byts_leidos] = '\0';
+		linea_sin_leer = ft_strjoin(linea_sin_leer, buffer);
 	}
-	free (buffer_temporal);
-	return (ft_obtener_linea(linea_sin_leer));
+	free(buffer);
+	linea_final = ft_obtener_linea(linea_sin_leer);
+	linea_sin_leer = ft_clean(linea_sin_leer);
+	return (linea_final);
 }
+
 
 int	main(void)
 {
 	int		fd;
-	char	buffer[1500];
-	size_t	bytes_leidos;
+	char	*linea;
 
 	fd = open("archivo.txt", O_RDONLY);
-	bytes_leidos = get_next_line(fd);
+	if (fd < 0)
+	{
+		printf("Error al abrir archivo.txt\n");
+		return (1);
+	}
+	while ((linea = get_next_line(fd)) != NULL)
+	{
+		printf("%s", linea);
+		free (linea); 
+	}
 	close(fd);
-
 	return (0);
 }
